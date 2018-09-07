1ff7038bcb5c87bbf9c86eaf8717217caf180a38
Eugene <32485871+eskhaliot@users.noreply.github.com>
2018-09-07 11:38:18 +1000

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main (String[] args){
+        double x, y, z;
+        x = 3;
+        y = 4;
+        z = Math.sqrt(x*x + y*y);
+        System.out.println(z);
+        //Comment
+    }
+}
